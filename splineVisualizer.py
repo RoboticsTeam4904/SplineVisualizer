fac8cc4b00d1cb4acfb442c545e36bc79f167806
Casey Manning <kcgrind@gmail.com>
2020-02-26 16:23:29 -0800

    top and bottom of robot

--- splineVisualizer.py
+++ splineVisualizer.py
@@ -3,7 +3,7 @@ import sys
 import time
 from networktables import NetworkTables
 import inspect
-
+import math
 # To see messages from networktables, you must setup logging
 import logging
 
@@ -18,11 +18,28 @@ NetworkTables.initialize(server=ip)
 
 sd = NetworkTables.getTable("splines")
 
-print(inspect.getmembers(sd, predicate=inspect.ismethod))
-
 xs = sd.getValue("splineY"),
 
 field_img = plt.imread("Field.png")
+
+robotWidth = 0.3  # in meters
+
+
+def getSides(xs, ys):
+    # for i in range(len(xs)):
+    angles = [math.atan2((ys[i+1] - ys[i]), (xs[i+1] - xs[i]))
+              for i in range(len(xs) - 1)]
+    print(angles)
+    angles.append(0)
+    bxs = [xs[i] + robotWidth * math.sin(angles[i]) for i in range(len(xs))]
+    bys = [ys[i] + robotWidth * math.cos(angles[i]) for i in range(len(ys))]
+    txs = [xs[i] - robotWidth * math.sin(angles[i]) for i in range(len(xs))]
+    tys = [ys[i] - robotWidth * math.cos(angles[i]) for i in range(len(ys))]
+    # for i in range(len(xs)):
+    #     txs[i] = xs[i]
+
+    return txs, tys, bxs, bys
+
 
 print(xs)
 while True:
@@ -32,18 +49,17 @@ while True:
     sxs = sd.getNumberArray("startX", [])
     sys = sd.getNumberArray("startY", [])
 
-    print("————————————————————————————————————————————————————————————————————————————")
-
-    print(xs)
-    print(ys)
-
-    print("————————————————————————————————————————————————————————————————————————————")
-
     if not len(xs) == 0:
         fig, ax = plt.subplots()
         ax.imshow(field_img, extent=(0, 16.48, 0, 8.1))
 
         plt.plot(xs, ys, '-')
+
+        topX, topY, bottomX, bottomY = getSides(xs, ys)
+
+        plt.plot(xs, ys, '-', c='#f39639')
+        plt.plot(topX, topY, '--', c='#f39639')
+        plt.plot(bottomX, bottomY, '--', c='#f39639')
 
         ax.scatter(sxs, sys, s=20, c='r')
 

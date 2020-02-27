9aa56dd41df50bdc7303ed4e6e95d60ae28c3522
Exr0n <spotyie@gmail.com>
2020-02-26 17:15:13 -0800

    add sine wave path, show plots

--- networktable-numbers/generator.py
+++ networktable-numbers/generator.py
@@ -1,4 +1,5 @@
 import numpy as np
+from matplotlib import pyplot as plt
 import json
 
 x_offset = 4
@@ -10,13 +11,22 @@ def generate_straight(x: float) -> float:
 def generate_right_turn(x: float) -> float:
     return np.log(x/5.0)/5.0 +1
 
-paths = {"straight": generate_straight, "right_turn": generate_right_turn}
+paths = {
+        "straight": generate_straight,
+        "right_turn": generate_right_turn,
+        "sine_wave": lambda x: np.sin(x*10)/2
+        }
 
 for path in paths:
     output = {"x": [], "y": []}
-    for i in np.arange(0.01, 2, 0.001):
+    for i in np.arange(0.01, 2, 0.01):
         output["x"].append(i + x_offset)
         output["y"].append(paths[path](i) + y_offset)
 
     with open(path + ".json", "w+") as wf:
         wf.write(json.dumps(output))
+
+    # plot it
+    plt.plot(output["x"], output["y"])
+    plt.show()
+

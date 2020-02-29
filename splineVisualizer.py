4f2105b2447089ae667edd1ddcb52b637e4f15ef
Casey Manning <kcgrind@gmail.com>
2020-02-28 16:36:21 -0800

    fix curve generation

--- splineVisualizer.py
+++ splineVisualizer.py
@@ -16,8 +16,8 @@ import logging
 
 # ip = sys.argv[1]
 data = None
-file_name = "networktable-numbers/straight.json"
-with open(file_name, "r") as rf: 
+file_name = "networktable-numbers/left_turn.json"
+with open(file_name, "r") as rf:
 
     data = json.loads(rf.read())
 
@@ -40,20 +40,66 @@ field_img = plt.imread("Field.png")
 robotWidth = 0.3  # in meters
 
 
-def getSides(xs, ys):
-    # for i in range(len(xs)):
-    angles = [math.atan2((ys[i+1] - ys[i]), (xs[i+1] - xs[i]))
-              for i in range(len(xs) - 1)]
-    print(angles)
-    angles.append(0)
-    bxs = [xs[i] + robotWidth * math.sin(angles[i]) for i in range(len(xs))]
-    bys = [ys[i] + robotWidth * math.cos(angles[i]) for i in range(len(ys))]
-    txs = [xs[i] - robotWidth * math.sin(angles[i]) for i in range(len(xs))]
-    tys = [ys[i] - robotWidth * math.cos(angles[i]) for i in range(len(ys))]
-    # for i in range(len(xs)):
-    #     txs[i] = xs[i]
+# def getSides(xs, ys):
+#     # for i in range(len(xs)):
+#     angles = [math.atan2((ys[i+1] - ys[i]), (xs[i+1] - xs[i]))
+#               for i in range(len(xs) - 1)]
+#     print(angles)
+#     angles.append(0)
+#     bxs = [xs[i] - robotWidth * math.sin(angles[i] + 90)
+#            for i in range(len(xs))]
+#     bys = [ys[i] - robotWidth * math.cos(angles[i] + 90)
+#            for i in range(len(ys))]
+#     txs = [xs[i] - robotWidth * math.sin(angles[i] - 90)
+#            for i in range(len(xs))]
+#     tys = [ys[i] - robotWidth * math.cos(angles[i] - 90)
+#            for i in range(len(ys))]
+#     # for i in range(len(xs)):
+#     #     txs[i] = xs[i]
 
-    return txs, tys, bxs, bys
+def offset(xs, ys, distance):
+    xsi = iter(xs)
+    ysi = iter(ys)
+    x1 = xs[0]
+    y1 = ys[0]
+    z = distance
+    points = []
+    rxs = []
+    rys = []
+    for i in range(len(xs)):
+        x2 = xs[i]
+        y2 = ys[i]
+        # tangential slope approximation
+        try:
+            slope = (y2 - y1) / (x2 - x1)
+            # perpendicular slope
+            # (might be 1/slope depending on direction of travel)
+            pslope = -1/slope
+        except ZeroDivisionError:
+            continue
+        mid_x = (x1 + x2) / 2
+        mid_y = (y1 + y2) / 2
+
+        sign = ((pslope > 0) == (x1 > x2)) * 2 - 1
+
+        # if z is the distance to your parallel curve,
+        # then your delta-x and delta-y calculations are:
+        #   z**2 = x**2 + y**2
+        #   y = pslope * x
+        #   z**2 = x**2 + (pslope * x)**2
+        #   z**2 = x**2 + pslope**2 * x**2
+        #   z**2 = (1 + pslope**2) * x**2
+        #   z**2 / (1 + pslope**2) = x**2
+        #   z / (1 + pslope**2)**0.5 = x
+
+        delta_x = sign * z / ((1 + pslope**2)**0.5)
+        delta_y = pslope * delta_x
+
+        points.append((mid_x + delta_x, mid_y + delta_y))
+        rxs.append(mid_x + delta_x)
+        rys.append(mid_y + delta_y)
+        x1, y1 = x2, y2
+    return rxs, rys
 
 
 # print(xs)
@@ -62,7 +108,6 @@ while True:
     xs = data["x"]
     # ys = sd.getNumberArray("splineY", [])
     ys = data["y"]
-
 
     # sxs = sd.getNumberArray("startX", [])
     # sys = sd.getNumberArray("startY", [])
@@ -74,7 +119,8 @@ while True:
         plt.plot(xs, ys, '-')
 
         # ax.scatter(sxs, sys, s=20, c='r')
-        topX, topY, bottomX, bottomY = getSides(xs, ys)
+        topX, topY = offset(xs, ys, robotWidth)
+        bottomX, bottomY = offset(xs, ys, -robotWidth)
 
         plt.plot(xs, ys, '-', c='#f39639')
         plt.plot(topX, topY, '--', c='#f39639')
